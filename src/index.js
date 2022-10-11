c6d7ac51704db92afec6a141a5b8eb4917bfe423
Tina Lacatis <danatina.dt@gmail.com>
2022-10-11 12:00:56 +0000

    add style and  images from google

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+import "./style.css";
+
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-const name = "Tina Lacatis";
-const year = new Date().getFullYear();
-
 root.render(
   <div>
-    <p> Created by {name} </p>
-    <p> Copyright {year} </p>
+    <h1 className="heading">My favourite foods</h1>
+    <div>
+      <img
+        className="food-img"
+        src="https://assets.bonappetit.com/photos/5b9a901947aaaf7cd9ea90f2/3:4/w_1998,h_2665,c_limit/ba-recipe-pasta-al-limone.jpg"
+        alt="pasta"
+      />
+      <img
+        className="food-img"
+        src="https://cdn.shopify.com/s/files/1/0410/4598/3397/articles/Margherita-9920.jpg?crop=center&height=800&v=1644589966&width=800"
+        alt="pizza"
+      />
+      <img
+        className="food-img"
+        src="https://www.cookingclassy.com/wp-content/uploads/2022/08/tiramisu-17.jpg"
+        alt="tiramisu"
+      />
+    </div>
   </div>
 );
