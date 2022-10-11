6c91dba68ce460d34c5b5f011c796e5e5e64c6dd
Tina Lacatis <danatina.dt@gmail.com>
2022-10-11 11:20:05 +0000

    write many constants in the same tag

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-const name = "Tina";
+const fName = "Tina";
+const lName = "Lacatis";
 const number = Math.round(Math.random() * 10);
 
 root.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>
+      Hello {fName + " " + lName}!
+      {/*
+      Hello {fName} {lName}!
+      Hello {`${fName} ${lName}`}! 
+      */}
+    </h1>
     <p>Your lucky number is {number}</p>
   </div>
 );
