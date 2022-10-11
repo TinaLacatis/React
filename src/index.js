dfd18f5d46df0d8978e59e7515ca909414323a2b
Tina Lacatis <danatina.dt@gmail.com>
2022-10-11 11:24:56 +0000

    add current year

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-const fName = "Tina";
-const lName = "Lacatis";
-const number = Math.round(Math.random() * 10);
+const name = "Tina Lacatis";
+const year = new Date().getFullYear();
 
 root.render(
   <div>
-    <h1>
-      Hello {fName + " " + lName}!
-      {/*
-      Hello {fName} {lName}!
-      Hello {`${fName} ${lName}`}! 
-      */}
-    </h1>
-    <p>Your lucky number is {number}</p>
+    <p> Created by {name} </p>
+    <p> Copyright {year} </p>
   </div>
 );
