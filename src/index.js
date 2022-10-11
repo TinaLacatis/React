ebecc7a986c0bcf05fa565d3e324bb545230b011
Tina Lacatis <danatina.dt@gmail.com>
2022-10-11 10:43:49 +0000

    add constants name, random number

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
+
+const name = "Tina";
+const number = Math.round(Math.random() * 10);
+
 root.render(
   <div>
-    <h1>My favourite foods</h1>
-    <ul>
-      <li>Pasta</li>
-      <li>Pizza</li>
-      <li>Soup</li>
-    </ul>
+    <h1>Hello {name}!</h1>
+    <p>Your lucky number is {number}</p>
   </div>
 );
