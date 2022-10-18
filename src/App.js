0a219574f302d55e353f35c12bb22fab46c50ab0
Tina Lacatis <danatina.dt@gmail.com>
2022-10-18 19:21:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {add, multiply, substract, divide} from "./calculator";
+import * as Calculator from "./calculator";
 
 export default function App() {
   return (
     <ul>
-      <li>{add(1, 2)}</li>
-      <li>{multiply(2, 3)}</li>
-      <li>{substract(7, 2)}</li>
-      <li>{divide(5, 2)}</li>
+      <li>{Calculator.add(1, 2)}</li>
+      <li>{Calculator.multiply(2, 3)}</li>
+      <li>{Calculator.substract(7, 2)}</li>
+      <li>{Calculator.divide(5, 2)}</li>
     </ul>
   );
 }
