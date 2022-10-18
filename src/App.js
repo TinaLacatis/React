8d2476f09e8f1d140c8573fe974869d3df775253
Tina Lacatis <danatina.dt@gmail.com>
2022-10-18 19:19:42 +0000

    import export calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import * as pi from "./math";
+import {add, multiply, substract, divide} from "./calculator";
 
 export default function App() {
   return (
     <ul>
-      <li>{pi.default}</li>
-      <li>{pi.doublePi()}</li>
-      <li>{pi.triplePi()}</li>
+      <li>{add(1, 2)}</li>
+      <li>{multiply(2, 3)}</li>
+      <li>{substract(7, 2)}</li>
+      <li>{divide(5, 2)}</li>
     </ul>
   );
 }
